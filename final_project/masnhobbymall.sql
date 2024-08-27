c160045f2aec16d59075f3e05b43b159d5b7cdbc
ghdudths <ghdudths1117@naver.com>
2024-08-27 17:34:20 +0900

    Revert "Merge branch '김원진Branch'"
    This reverts commit 8f3a094d5ad4859353fbdbc6a61f2e370f64edbe, reversing
    changes made to 53407427f871f52fa4301092fda98bef2fa043b9.

--- final_project/masnhobbymall.sql
+++ final_project/masnhobbymall.sql
@@ -11,7 +11,6 @@ DROP TABLE IF EXISTS Orders;
 DROP TABLE IF EXISTS DStatus;
 DROP TABLE IF EXISTS Product;
 DROP TABLE IF EXISTS DCategory;
-DROP TABLE IF EXISTS MCategory;
 DROP TABLE IF EXISTS Category;
 DROP TABLE IF EXISTS CouponBox;
 DROP TABLE IF EXISTS Coupon;
@@ -21,27 +20,18 @@ DROP TABLE IF EXISTS Email;
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Grade;
 
+
 -- Grade 테이블
 CREATE TABLE Grade (
-    gIdx int PRIMARY KEY AUTO_INCREMENT,
+    gIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     gradeName varchar(200) NOT NULL,
     authority int NOT NULL,
     discount int NOT NULL
 );
 
--- Grade 테이블에 샘플 데이터 삽입
-INSERT INTO Grade (gradeName, authority, discount)
-VALUES (‘Bronze’, 1, 5), 
-       (‘Silver’, 2, 10),
-       (‘Gold’, 3, 15), 
-       (‘Platinum’, 4, 20);
--- 등급 데이터 삭제
-DELETE FROM Grade WHERE gradeName = 'Bronze';
-DELETE FROM Grade;
-
 -- User 테이블
 CREATE TABLE User (
-    userIdx int PRIMARY KEY AUTO_INCREMENT,
+    userIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     gIdx int NOT NULL DEFAULT 1,
     id varchar(50) NOT NULL,
     password varchar(50) NOT NULL,
@@ -53,23 +43,13 @@ CREATE TABLE User (
     point bigint NOT NULL DEFAULT 0,
     createAt DATETIME NOT NULL
 );
-
--- User 테이블에 샘플 데이터 삽입
-INSERT INTO User (gIdx, id, password, nickName, name, phone, addr, adminAt, point, createAt)
-VALUES 
-(1, ‘test’, ‘test’, ‘테스트‘, ‘테스트’, ‘010-1234-5678’, ‘Address 1’, ‘N’, 1, NOW()),
-(4, ‘admin’, ‘admin’, ‘관리자‘, ‘관리자’, ‘010-1111-1111’, ‘서울시 관악구’, ‘Y’, 1000000, NOW());
--- 유저 데이터 삭제
-DELETE FROM User WHERE gIdx = 1;
-DELETE FROM User;
-
 -- User 테이블과 Grade 테이블의 외래 키
 ALTER TABLE User
 ADD CONSTRAINT FK_Grade_TO_User FOREIGN KEY (gIdx) REFERENCES Grade (gIdx);
 
 -- Email 테이블
 CREATE TABLE Email (
-    emailIdx int PRIMARY KEY AUTO_INCREMENT,
+    emailIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userIdx int NOT NULL,
     email varchar(50) NULL,
     esite varchar(20) NOT NULL
@@ -80,7 +60,7 @@ ADD CONSTRAINT FK_User_TO_Email FOREIGN KEY (userIdx) REFERENCES User (userIdx);
 
 -- DAddress 테이블
 CREATE TABLE DAddress (
-    daIdx int PRIMARY KEY AUTO_INCREMENT,
+    daIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userIdx int NOT NULL,
     daPhone varchar(20) NOT NULL,
     daAddr LONGTEXT NOT NULL
@@ -91,7 +71,7 @@ ADD CONSTRAINT FK_User_TO_DAddress FOREIGN KEY (userIdx) REFERENCES User (userId
 
 -- GCondition 테이블
 CREATE TABLE GCondition (
-    jIdx int PRIMARY KEY AUTO_INCREMENT,
+    jIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userIdx int NOT NULL,
     bpAmount BIGINT NOT NULL DEFAULT 0,
     rCount int NOT NULL DEFAULT 0,
@@ -103,7 +83,7 @@ ADD CONSTRAINT FK_User_TO_GCondition_1 FOREIGN KEY (userIdx) REFERENCES User (us
 
 -- Coupon 테이블
 CREATE TABLE coupon (
-    cIdx int PRIMARY KEY AUTO_INCREMENT,
+    cIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     cName varchar(60) NOT NULL,
     discount int NOT NULL,
     dcType char(1) NOT NULL DEFAULT '-'
@@ -111,7 +91,7 @@ CREATE TABLE coupon (
 
 -- CouponBox 테이블
 CREATE TABLE CouponBox (
-    cbIdx int PRIMARY KEY AUTO_INCREMENT,
+    cbIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userIdx int NOT NULL,
     cIdx int NOT NULL,
     useAt char(1) NOT NULL DEFAULT 'N'
@@ -125,33 +105,23 @@ ADD CONSTRAINT FK_coupon_TO_CouponBox FOREIGN KEY (cIdx) REFERENCES Coupon (cIdx
 
 -- Category 테이블
 CREATE TABLE Category (
-    categoryNo int PRIMARY KEY AUTO_INCREMENT,
+    categoryNo int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     categoryName varchar(200) NOT NULL
 );
 
--- MCategory 테이블
-CREATE TABLE MCategory (
-    mcategoryNo int PRIMARY KEY AUTO_INCREMENT,
-    categoryNo int NOT NULL,
-    mcategoryName varchar(200) NOT NULL
-);
--- MCategory 테이블과 Category 테이블의 외래 키
-ALTER TABLE MCategory
-ADD CONSTRAINT FK_Category_TO_MCategory FOREIGN KEY (categoryNo) REFERENCES Category (categoryNo);
-
 -- DCategory 테이블
 CREATE TABLE DCategory (
-    dcategoryNo int PRIMARY KEY AUTO_INCREMENT,
-    mcategoryNo int NOT NULL,
+    dcategoryNo int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    categoryNo int NOT NULL,
     dcategoryName varchar(200) NOT NULL
 );
 -- DCategory 테이블과 Category 테이블의 외래 키
 ALTER TABLE DCategory
-ADD CONSTRAINT FK_MCategory_TO_DCategory FOREIGN KEY (mcategoryNo) REFERENCES MCategory (mcategoryNo);
+ADD CONSTRAINT FK_Category_TO_DCategory FOREIGN KEY (categoryNo) REFERENCES Category (categoryNo);
 
 -- Product 테이블
 CREATE TABLE Product (
-    pIdx int PRIMARY KEY AUTO_INCREMENT,
+    pIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     categoryNo int NOT NULL,
     dcategoryNo int NOT NULL,
     pName varchar(200) NOT NULL,
@@ -168,14 +138,14 @@ ADD CONSTRAINT FK_DCategory_TO_Product FOREIGN KEY (dcategoryNo) REFERENCES DCat
 
 -- DStatus 테이블
 CREATE TABLE DStatus (
-    dsIdx int PRIMARY KEY AUTO_INCREMENT,
+    dsIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     dsType varchar(30) NOT NULL,
     dsContent LONGTEXT NOT NULL
 );
 
 -- Orders 테이블
 CREATE TABLE Orders (
-    oIdx int PRIMARY KEY AUTO_INCREMENT,
+    oIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     dsIdx int NOT NULL,
     usepoint BIGINT NOT NULL DEFAULT 0,
     oDate DATETIME NOT NULL DEFAULT now(),
@@ -187,7 +157,7 @@ ADD CONSTRAINT FK_DStatus_TO_Orders FOREIGN KEY (dsIdx) REFERENCES DStatus (dsId
 
 -- BuyList 테이블
 CREATE TABLE BuyList (
-    bIdx int PRIMARY KEY AUTO_INCREMENT,
+    bIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userIdx int NOT NULL,
     pIdx int NOT NULL,
     oIdx int NOT NULL,
@@ -206,7 +176,7 @@ ADD CONSTRAINT FK_Orders_TO_BuyList FOREIGN KEY (oIdx) REFERENCES Orders (oIdx);
 
 -- SCart 테이블
 CREATE TABLE SCart (
-    scIdx int PRIMARY KEY AUTO_INCREMENT,
+    scIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userIdx int NOT NULL,
     pIdx int NOT NULL,
     scamount int NOT NULL DEFAULT 1
@@ -220,7 +190,7 @@ ADD CONSTRAINT FK_Product_TO_SCart FOREIGN KEY (pIdx) REFERENCES Product (pIdx);
 
 -- Review 테이블
 CREATE TABLE review (
-    rvIdx int PRIMARY KEY AUTO_INCREMENT,
+    rvIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     pIdx int NOT NULL,
     userIdx int NOT NULL,
     rvContent LONGTEXT NOT NULL,
@@ -235,7 +205,7 @@ ADD CONSTRAINT FK_User_TO_Review FOREIGN KEY (userIdx) REFERENCES User (userIdx)
 
 -- Follow 테이블
 CREATE TABLE Follow (
-    fIdx int PRIMARY KEY AUTO_INCREMENT,
+    fIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     rvIdx int NOT NULL,
     userIdx int NOT NULL,
     followDate DATETIME NOT NULL DEFAULT now()
@@ -249,7 +219,7 @@ ADD CONSTRAINT FK_User_TO_Follow_1 FOREIGN KEY (userIdx) REFERENCES User (userId
 
 -- Inquiry 테이블
 CREATE TABLE Inquiry (
-    inIdx int PRIMARY KEY AUTO_INCREMENT,
+    inIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     pIdx int NOT NULL,
     userIdx int NOT NULL,
     inType varchar(30) NOT NULL DEFAULT '기타',
@@ -266,7 +236,7 @@ ADD CONSTRAINT FK_User_TO_Inquiry_1 FOREIGN KEY (userIdx) REFERENCES User (userI
 
 -- Answer 테이블
 CREATE TABLE Answer (
-    aIdx int PRIMARY KEY AUTO_INCREMENT,
+    aIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     inIdx int NOT NULL,
     userIdx int NOT NULL,
     aContent LONGTEXT NOT NULL,
@@ -281,7 +251,7 @@ ADD CONSTRAINT FK_Inquiry_TO_Answer_3 FOREIGN KEY (userIdx) REFERENCES Inquiry (
 
 -- Compony 테이블
 CREATE TABLE Compony (
-    comIdx int PRIMARY KEY AUTO_INCREMENT,
+    comIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     comName varchar(200) NOT NULL,
     comAddr LONGTEXT NOT NULL,
     comPhone varchar(50) NOT NULL
@@ -289,7 +259,7 @@ CREATE TABLE Compony (
 
 -- Chat_logs 테이블
 CREATE TABLE Chat_logs (
-    chatIdx int PRIMARY KEY AUTO_INCREMENT,
+    chatIdx int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userId varchar(50) NULL,
     startTime DATETIME NOT NULL DEFAULT now(),
     inputMode char(1) NOT NULL DEFAULT 'T',
