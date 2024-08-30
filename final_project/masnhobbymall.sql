1dc7e2866c9ebce5b909a52048f96a7d7a7f6a7d
ghdudths <ghdudths1117@naver.com>
2024-08-30 16:16:00 +0900

    sql 백업용

--- final_project/masnhobbymall.sql
+++ final_project/masnhobbymall.sql
@@ -299,3 +299,156 @@ CREATE TABLE Chat_logs (
     responseText LONGTEXT NOT NULL,
     endTime DATETIME NOT NULL
 );
+
+-- Grade 테이블에 샘플 데이터 삽입
+INSERT INTO Grade (gradeName, authority, discount)
+VALUES ('Bronze', 1, 5),
+       ('Silver', 2, 10),
+       ('Gold', 3, 15),
+       ('Platinum', 4, 20);
+-- User 테이블에 샘플 데이터 삽입
+INSERT INTO User (gIdx, id, password, nickName, name, phone, addr, adminAt, point, createAt)
+VALUES
+(2, 'test', 'test', '테스트', '테스트', '010-1111-2222', 'Address 1', 'N', 100, NOW()),
+(4, 'admin', 'admin', '관리자', '관리자', '010-1111-1111', '서울시 관악구', 'Y', 100000, NOW());
+
+
+-- 대 카테고리 데이터 추가
+INSERT INTO category values(null,'게임');
+INSERT INTO category values(null,'스포츠');
+
+-- 중 카테고리 데이터 추가 게임
+INSERT INTO mcategory values(null,1,'키보드');
+INSERT INTO mcategory values(null,1,'마우스');
+INSERT INTO mcategory values(null,1,'노트북');
+INSERT INTO mcategory values(null,1,'기타');
+-- 중 카테고리 데이터 추가 스포츠
+INSERT INTO mcategory values(null,2,'축구');
+INSERT INTO mcategory values(null,2,'야구');
+INSERT INTO mcategory values(null,2,'농구');
+INSERT INTO mcategory values(null,2,'기타');
+select * from mcategory where categoryNo = 2;
+
+-- 소 카테고리 데이터 추가  
+-- 키보드
+INSERT INTO dcategory values(null,1,'키보드패드');
+INSERT INTO dcategory values(null,1,'기계식키보드');
+INSERT INTO dcategory values(null,1,'멤브레인키보드');
+INSERT INTO dcategory values(null,1,'게이밍키보드');
+
+-- 마우스
+INSERT INTO dcategory values(null,2,'손목받침대');
+INSERT INTO dcategory values(null,2,'마우스패드');
+INSERT INTO dcategory values(null,2,'손목보호마우스');
+INSERT INTO dcategory values(null,2,'게이밍마우스');
+
+-- 스포츠 축구
+INSERT INTO dcategory values(null,5,'축구화');
+INSERT INTO dcategory values(null,5,'축구공');
+INSERT INTO dcategory values(null,5,'의상');
+INSERT INTO dcategory values(null,5,'기타');
+
+-- 스포츠 야구
+INSERT INTO dcategory values(null,6,'야구배트');
+INSERT INTO dcategory values(null,6,'야구공');
+INSERT INTO dcategory values(null,6,'글러브');
+INSERT INTO dcategory values(null,6,'기타');
+select * from dcategory; 
+-- 상품 데이터 추가 대 1=게임 2=스포츠 / 중 1=키보드 2=마우스 5=축구 6=야구 / 소 1~8까지 게임카테고리 9~16까지 스포츠카테고리
+-- 소카테고리 하나당 상품 3개씩 키보드패드
+insert into product values(null, 1, 1, 1, '각청 장패드', '각청이 그려진 장패드', 5, 50000);
+insert into product values(null, 1, 1, 1, '일반 장패드', '꾸밈 없는 장패드', 6, 20000);
+insert into product values(null, 1, 1, 1, '커스텀 장패드', '원하는 그림이 그려진 장패드', 7, 80000);
+
+-- 소카테고리 하나당 상품 3개씩 기계식키보드
+insert into product values(null, 1, 1, 2, '기계식키보드1', '흑축키보드', 3, 170000);
+insert into product values(null, 1, 1, 2, '기계식키보드2', '갈축키보드', 1, 80000);
+insert into product values(null, 1, 1, 2, '기계식키보드3', '적축키보드', 4, 110000);
+
+-- 소카테고리 하나당 상품 3개씩 멤브레인키보드
+insert into product values(null, 1, 1, 3, '멤브레인키보드1', '가격이 저렴한 멤브레인키보드1', 17, 20000);
+insert into product values(null, 1, 1, 3, '멤브레인키보드2', '가격이 저렴한 멤브레인키보드2', 14, 10000);
+insert into product values(null, 1, 1, 3, '멤브레인키보드3', '기계식인척하는 멤브레인키보드', 4, 40000);
+
+-- 소카테고리 하나당 상품 3개씩 게이밍키보드
+insert into product values(null, 1, 1, 4, '게이밍키보드1', '아무튼 반짝이는 화려한 키보드1', 7, 70000);
+insert into product values(null, 1, 1, 4, '게이밍키보드2', '아무튼 반짝이는 화려한 키보드2', 4, 200000);
+insert into product values(null, 1, 1, 4, '게이밍키보드3', '아무튼 반짝이는 화려한 키보드3', 3, 120000);
+
+-- 소카테고리 하나당 상품 3개씩
+insert into product values(null, 1, 2, 5, '실리콘 손목받침대', '실리콘 손목받침대', 3, 5000);
+insert into product values(null, 1, 2, 5, '원목 손목받침대', '원목 손목받침대', 3, 20000);
+
+-- 소카테고리 하나당 상품 3개씩
+insert into product values(null, 1, 2, 6, '라이덴 마우스패드', '쇼군이 그려진 마우스패드', 5, 40000);
+insert into product values(null, 1, 2, 6, '일반 마우스패드', '그냥 마우스패드', 5, 10000);
+insert into product values(null, 1, 2, 6, '대형 마우스패드', 'fps게임에 유리한 대형 마우스패드', 5, 20000);
+
+-- 소카테고리 하나당 상품 3개씩
+insert into product values(null, 1, 2, 7, '손목보호 마우스1', '인체공학적으로 만들어진 손목보호마우스1', 5, 30000);
+insert into product values(null, 1, 2, 7, '손목보호 마우스2', '인체공학적으로 만들어져 야근에 적합한 손목보호마우스2', 5, 40000);
+insert into product values(null, 1, 2, 7, '손목보호 업무용 마우스3', '인체공학적으로 만들어져 사무용으로 적합한 손목보호마우스3', 5, 25000);
+
+-- 소카테고리 하나당 상품 3개씩
+insert into product values(null, 1, 2, 8, '게이밍마우스1', '반응이 빠른 게이밍 마우스', 5, 70000);
+insert into product values(null, 1, 2, 8, '게이밍마우스2', '반응이 빠르고 내구성이 좋은 게이밍 마우스', 5, 170000);
+insert into product values(null, 1, 2, 8, '게이밍마우스1', '내구성이 좋은 게이밍 마우스', 5, 100000);
+
+-- 소카테고리 하나당 상품 3개씩
+insert into product values(null, 2, 5, 9, '유명한 축구화', '대중적인 축구화', 30, 210000);
+insert into product values(null, 2, 5, 9, '코난 운동화', '다이얼을 돌리고 차면 10배 강하게 나간다', 1, 21000000);
+insert into product values(null, 2, 5, 9, '그냥 축구화', '그냥 축구화', 3, 100000);
+
+-- 소카테고리 하나당 상품 3개씩 
+insert into product values(null, 2, 5, 10, '그냥 축구공', '그냥 축구공', 3, 20000);
+insert into product values(null, 2, 5, 10, '자블라니', '개같은 축구공', 3, 50000);
+insert into product values(null, 2, 5, 10, '바람 넣는 펌프', '공에 바람을 넣는 펌프', 3, 20000);
+
+-- 소카테고리 하나당 상품 3개씩 
+insert into product values(null, 2, 5, 11, '토트넘 유니폼', '등번호 7번', 3, 200000);
+insert into product values(null, 2, 5, 11, '체육복', '아무데나 입기 좋은 트레이닝복 세트', 3, 40000);
+insert into product values(null, 2, 5, 11, '양말', '시멘트 바닥에 굴러도 끄떡없는 양말', 3, 10000);
+
+-- 소카테고리 하나당 상품 3개씩 
+insert into product values(null, 2, 5, 12, '기타상품', '나도몰라잉', 1, 1000);
+
+-- 소카테고리 하나당 상품 3개씩 
+insert into product values(null, 2, 6, 13, '알루미늄배트', '부산행 찍기 딱 좋은 빠따', 4, 51000);
+insert into product values(null, 2, 6, 13, '나무배트', '홈런 치기 딱 좋은 빠따', 4, 101000);
+insert into product values(null, 2, 6, 13, '티타늄배트', '손목 박살나기 딱 좋은 빠따', 4, 201000);
+
+-- 소카테고리 하나당 상품 3개씩 
+insert into product values(null, 2, 6, 14, '그냥 야구공', '언제나 쓰기 적당한 야구공', 4, 20000);
+insert into product values(null, 2, 6, 14, '말랑한 야구공', '애들이 갖고 놀아도 안전한 야구공', 4, 30000);
+insert into product values(null, 2, 6, 14, '장식용 가짜 싸인볼', '그냥 집에 장식하는 용도', 4, 10000);
+
+-- 소카테고리 하나당 상품 3개씩 
+insert into product values(null, 2, 6, 15, '그냥 글러브', '누구나 쓰기 좋은 글러브', 4, 31000);
+insert into product values(null, 2, 6, 15, '튼튼한 글러브', '내구성이 좋은 글러브', 4, 101000);
+insert into product values(null, 2, 6, 15, '아동용 글러브', '아이들이 쓰기 좋은 글러브', 4, 21000);
+
+-- 소카테고리 하나당 상품 3개씩 
+insert into product values(null, 2, 6, 16, '기타', '모자?', 4, 100);
+
+
+CREATE OR REPLACE
+    ALGORITHM = UNDEFINED 
+    DEFINER = `final`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `shop_list_view` AS
+    SELECT 
+        `p`.`pIdx` AS `pIdx`,
+        `p`.`categoryNo` AS `categoryNo`,
+        `p`.`mcategoryNo` AS `mcategoryNo`,
+        `p`.`dcategoryNo` AS `dcategoryNo`,
+        `p`.`pName` AS `pName`,
+        `p`.`pEx` AS `pEx`,
+        `p`.`price` AS `price`,
+        `c`.`categoryName` AS `categoryName`,
+        `m`.`mcategoryName` AS `mcategoryName`,
+        `d`.`dcategoryName` AS `dcategoryName`
+    FROM
+        (((`product` `p`
+        JOIN `category` `c` ON ((`p`.`categoryNo` = `c`.`categoryNo`)))
+        JOIN `mcategory` `m` ON ((`p`.`mcategoryNo` = `m`.`mcategoryNo`)))
+        JOIN `dcategory` `d` ON ((`p`.`dcategoryNo` = `d`.`dcategoryNo`)))
