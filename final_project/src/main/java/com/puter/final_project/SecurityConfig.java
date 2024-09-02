ccb8215c15bf25871e2ad3e44e69df97d3596fc4
심우림 <msa1530@naver.com>
2024-09-02 12:08:20 +0900

    Feat : 구글 간편 로그인 토큰만 확인

---
+++ final_project/src/main/java/com/puter/final_project/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.puter.final_project;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                    .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> 
+                oauth2.defaultSuccessUrl("/home.do", true)
+            );
+        return http.build();
+    }
+}
