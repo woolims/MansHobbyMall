5b380d8316ec08f75d710085b03f84397d9fa43b
dnjswls <dnjswls6670@naver.com>
2024-09-23 10:05:53 +0900

    feat : 수량 변경시 최종결제금액 실시간 변경 기능

--- final_project/src/main/java/com/puter/final_project/controller/PaymentControllor.java
+++ final_project/src/main/java/com/puter/final_project/controller/PaymentControllor.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.puter.final_project.dao.CartMapper;
